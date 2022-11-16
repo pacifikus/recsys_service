e2256d50ddef9a0279a07612c64ad1147410c501
pacificus <masterkristall@gmail.com>
2022-11-16 21:23:51 +0700

    Make available on windows

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ app = create_app(config)
 if __name__ == "__main__":
 
     host = os.getenv("HOST", "127.0.0.1")
-    port = int(os.getenv("PORT", "8080"))
+    port = int(os.getenv("PORT", "5000"))
 
     uvicorn.run(app, host=host, port=port)
