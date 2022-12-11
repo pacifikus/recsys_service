d9c0a69e2cc2fb7e133b67feb22c16ddfae3df2f
pacificus <masterkristall@gmail.com>
2022-12-12 02:21:50 +0700

    Feat: hybrid model

--- src/recommenders/__init__.py
+++ src/recommenders/__init__.py
@@ -1,9 +1,6 @@
+from .hybrid import HybridModelrapper
 from .lightfm import LightFMWrapper
 from .most_popular import MostPopularRecommender
 from .user_knn import UserKNN
 
-__all__ = [
-    "MostPopularRecommender",
-    "UserKNN",
-    "LightFMWrapper",
-]
+__all__ = ["MostPopularRecommender", "UserKNN", "LightFMWrapper", "HybridModelrapper"]
